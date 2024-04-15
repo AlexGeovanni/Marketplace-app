30eeb587fc83c2a6665b30956a11245f466bdc30
Alex Geovanni <106128149+AlexGeovanni@users.noreply.github.com>
2024-04-15 12:22:45 -0700

    Actualizar Index.jsx
    Home

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -4,8 +4,19 @@ export default function Home(){
     return(
         <>
             <Header />
-            <section className="bg-danger">
-                <h1>Hola</h1>
+            <section className="bg-danger hv-100">
+                <div className="d-flex align-itemes-center
+                    jutify-content-start"> 
+                <h1>marketplace</h1>
+                <div className="mt-3">
+                 <p>Solo para Tijuana,BC.</p>
+                 <button type="button" 
+                    className+"btn btn-primary mt-1">
+                    Registrate
+                 </button>
+                  
+                </div>
+                </div>
             </section>
         </>
     )
