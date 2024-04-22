01dbdddb4e382ebbc80169d2f335d8c784f9aa5e
Alex G <A180065@unach.mx>
2024-04-22 11:32:59 -0700

    mejorando las nuevas paginas 1.4

--- src/components/Home/Product-off.jsx
+++ src/components/Home/Product-off.jsx
@@ -5,15 +5,15 @@ import img1 from "../../assets/img-13.jpeg";
 import { useEffect, useState } from "react";
 
 export default function ProductOff() {
-    const [width,setWidth]=useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-        window.addEventListener("resize", handleResize)
-    }, []);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
 
-    const handleResize = () => {
-        setWidth(window.innerWidth);
-    };
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
 
   const product = [
     {
@@ -42,48 +42,45 @@ export default function ProductOff() {
       off: "15",
     },
     {
-        id: 1,
-        name: "Gorra6",
-        off: "15",
-      },
+      id: 1,
+      name: "Gorra6",
+      off: "15",
+    },
     {
-        id: 1,
-        name: "Gorra7",
-        off: "15",
-      
-    }
+      id: 1,
+      name: "Gorra7",
+      off: "15",
+    },
   ];
-//   slidesPerView={width <768 ? 2 : width < 992 && width >= 768? 3 : 5}
-//   spaceBetween={width <768 ? 6 :10}
+  //   slidesPerView={width <768 ? 2 : width < 992 && width >= 768? 3 : 5}
+  //   spaceBetween={width <768 ? 6 :10}
 
   return (
     <>
       <section className="seccions m-auto px-3 px-lg-4 mt-3 mt-md-5">
         <h2>Local-marketplace ofertas</h2>
         <Swiper
-          slidesPerView={width <768 ? 2.2 : width < 992 && width >= 768? 3 : 5}
-          spaceBetween={width <768 ? 6 :10}
+          slidesPerView={
+            width < 768 ? 2.2 : width < 992 && width >= 768 ? 3 : 5
+          }
+          spaceBetween={width < 768 ? 6 : 10}
           loop={true}
           navigation={true}
           modules={[Pagination, Navigation]}
           className="mySwiper"
         >
-          {
-            product.map((item) => {
-                return (
-                  <SwiperSlide key={item.name}>
-                    <div className="cp card ">
-                      <img
-                        className=""
-                        src={img1}
-                        alt=""
-                      />
-                      <p className=" m-0 text-center">{item.name}</p>
-                    </div>
-                  </SwiperSlide>
-                );
-              })
-          }
+          {product.map((item) => {
+            return (
+              <SwiperSlide key={item.name}>
+                <div className="cp card-carousel ">
+                  <div className="card-img">
+                    <img className="" src={img1} alt="" />
+                  </div>
+                  <p className=" m-0 text-center">{item.name}</p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
       </section>
     </>
